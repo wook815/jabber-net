dba032e79b614509efa34c2f2a4ee8d959e6308b
smorris92 <smorris92@b87ccc53-a82b-0410-9262-3dc31cef668b>
2008-02-20 23:43:33 +0000

    Tagged from JabberNet #18

---
+++ tags/JabberNet-18/test/jabber/protocol/client/PresenceTest.cs
@@ -0,0 +1,53 @@
+/* --------------------------------------------------------------------------
+ * Copyrights
+ *
+ * Portions created by or assigned to Cursive Systems, Inc. are
+ * Copyright (c) 2002-2008 Cursive Systems, Inc.  All Rights Reserved.  Contact
+ * information for Cursive Systems, Inc. is available at
+ * http://www.cursive.net/.
+ *
+ * License
+ *
+ * Jabber-Net can be used under either JOSL or the GPL.
+ * See LICENSE.txt for details.
+ * --------------------------------------------------------------------------*/
+using System;
+
+using System.Xml;
+using NUnit.Framework;
+
+using bedrock.util;
+using jabber.protocol.client;
+
+namespace test.jabber.protocol.client
+{
+    /// <summary>
+    /// Summary description for PresenceTest.
+    /// </summary>
+    [SVN(@"$Id$")]
+    [TestFixture]
+    public class PresenceTest
+    {
+        XmlDocument doc = new XmlDocument();
+        [Test] public void Test_Create()
+        {
+            Presence p = new Presence(doc);
+            p.Type   = PresenceType.available;
+            p.Status = "foo";
+            Assert.AreEqual("<presence><status>foo</status></presence>", p.ToString());
+        }
+
+        [Test] public void Test_Available()
+        {
+            Presence p = new Presence(doc);
+            Assert.AreEqual(PresenceType.available, p.Type);
+            Assert.AreEqual("", p.GetAttribute("type"));
+            p.Type = PresenceType.unavailable;
+            Assert.AreEqual(PresenceType.unavailable, p.Type);
+            Assert.AreEqual("unavailable", p.GetAttribute("type"));
+            p.Type = PresenceType.available;
+            Assert.AreEqual(PresenceType.available, p.Type);
+            Assert.AreEqual("", p.GetAttribute("type"));
+        }
+    }
+}
